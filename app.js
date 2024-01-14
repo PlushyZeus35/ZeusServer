3a5f5f1cff75ea7c4bbe55a716cf86928063657c
Borja Lorenzo <plushyzeus35@gmail.com>
2024-01-14 19:33:07 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path')
+
+// INITIALIZATIONS
+const app = express();
+
+
+// SETTINGS
+// Set static path to serve static files
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+// TEMPLATE ENGINE
+
+// MIDDLEWARES
+
+// GLOBAL VARIABLES
+
+// ROUTES
+app.use(require('./routes'));
+app.use(require('./routes/index'));
+
+
+module.exports = app;
